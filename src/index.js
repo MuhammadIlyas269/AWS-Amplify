d740e47aedf54c648b2e0a3d70fa93398436a7ae
Muhammad Ilyas <MuhammadIlyas523221@gmail.com>
2023-05-31 17:54:25 +0500

    Amplify Continous Demployment check

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ root.render(
   </AmplifyProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measuring performance in your app, pass
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
